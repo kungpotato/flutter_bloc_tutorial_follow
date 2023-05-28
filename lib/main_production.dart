e83b187308488537ac03812aee7a20ec9fba0859
KUNGPOTATO\kungp <kittisak.py@gmail.com>
2023-05-28 14:12:47 +0700

    kk

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,38 +1,5 @@
-import 'dart:async';
-import 'dart:developer';
+import 'package:fluttertodo/bootstrap.dart';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:fluttertodo/simple_bloc_observer.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
-
-import 'app.dart';
-import 'counter_observer.dart';
-
-void main() async {
-  runZonedGuarded(
-    () async {
-      FlutterError.onError = (details) {
-        log(details.exceptionAsString(), stackTrace: details.stack);
-      };
-
-      HydratedBloc.storage = await HydratedStorage.build(
-        storageDirectory: kIsWeb
-            ? HydratedStorage.webStorageDirectory
-            : await getTemporaryDirectory(),
-      );
-
-      Bloc.observer = const CounterObserver();
-      Bloc.observer = const SimpleBlocObserver();
-      // Bloc.observer = const AppBlocObserver();
-
-      // final todosRepository = TodosRepository(todosApi: todosApi);
-      BindingBase.debugZoneErrorsAreFatal = true;
-      WidgetsFlutterBinding.ensureInitialized();
-
-      return const App();
-    },
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
-  );
+void main() {
+  bootstrap();
 }
